a8c4d6aacc974389b36772104c6eef838b93aba4
Juan Pineda <jpinedaespino@gmail.com>
2019-11-07 20:39:53 -0600

    Login & Register Design

---
+++ src/proyecto2/EDDProyecto2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+import Interfaz.LoginFrame;
+
+/**
+ *
+ * @author brest12
+ */
+public class EDDProyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginFrame f = new LoginFrame();
+        f.setLocationRelativeTo(null);
+        f.show();
+    }
+    
+}
---
+++ src/proyecto2/Objetos/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2.Objetos;
+
+
+public class Usuario {
+    
+    private String userName;
+    private String password;
+    private String timeStamp;
+    
+}
